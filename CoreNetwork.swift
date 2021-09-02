eca9b19aa13df9d42766408fee4ad061694fe468
Mostafa Mazrouh <mostafa.mazrouh@svea.com>
2021-09-02 15:49:40 +0200

    First commit

---
+++ CoreNetwork.swift
@@ -0,0 +1,23 @@
+//
+//  CoreNetwork.swift
+//  CoreNetwork
+//
+//  Created by Mostafa Mazrouh on 2021-08-31.
+//
+
+import Foundation
+
+
+public class CoreNetwork {
+    
+    public init() {
+    }
+    
+    public func log() {
+        print("Hello CoreNetwork")
+    }
+}
+
+
+
+
--- Example/CoreNetwork/ViewController.swift
+++ Example/CoreNetwork/ViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import CoreNetwork
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let coreNetwork = CoreNetwork()
+        coreNetwork.log()
+        
     }
 
     override func didReceiveMemoryWarning() {
